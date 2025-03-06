4abcc863abbc016bf872aa1e37d7fe80daf9cc1a
Alison_Loffi <alison.loffi@areacentral.com.br>
2025-03-06 09:06:35 -0300

    Adicionado PWA

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "calculadora-pwa-v1";
+const urlsToCache = [
+  "/index.html",
+  "/manifest.json",
+  "/service-worker.js",
+  "https://unpkg.com/@tailwindcss/browser@4"
+];
+
+// Instala o Service Worker e faz o cache dos arquivos
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Intercepta requisições e responde com o cache
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Ativa o novo Service Worker e remove caches antigos
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cache) => cache !== CACHE_NAME)
+          .map((cache) => caches.delete(cache))
+      );
+    })
+  );
+});
